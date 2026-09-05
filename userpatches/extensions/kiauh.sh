5a5576dbe6a46d3efd12a341140d29d746afaf7d
kerbilg <44060099+kerbilg@users.noreply.github.com>
2026-09-06 00:14:26 +0200

    fix: unbuffer

--- userpatches/extensions/kiauh.sh
+++ userpatches/extensions/kiauh.sh
@@ -6,12 +6,14 @@ function post_customize_image__010_download_kiauh() {
 }
 
 function post_customize_image__011_install_stack() {
-    chroot_sdcard_apt_get_update    
-    chroot_sdcard env -u COLUMNS sudo -H -u fluidd /home/fluidd/kiauh/kiauh.sh install klipper --count 1 --create-example-cfg
+    chroot_sdcard_apt_get_update
+    chroot_sdcard_apt_get_install expect
+    
+    chroot_sdcard unbuffer sudo -H -u fluidd /home/fluidd/kiauh/kiauh.sh install klipper --count 1 --create-example-cfg
 
-    chroot_sdcard env -u COLUMNS sudo -H -u fluidd /home/fluidd/kiauh/kiauh.sh install moonraker
+    chroot_sdcard unbuffer sudo -H -u fluidd /home/fluidd/kiauh/kiauh.sh install moonraker
     
-    chroot_sdcard env -u COLUMNS sudo -H -u fluidd /home/fluidd/kiauh/kiauh.sh install fluidd
+    chroot_sdcard unbuffer sudo -H -u fluidd /home/fluidd/kiauh/kiauh.sh install fluidd
     
-    chroot_sdcard env -u COLUMNS sudo -H -u fluidd /home/fluidd/kiauh/kiauh.sh install fluidd-config
+    chroot_sdcard unbuffer sudo -H -u fluidd /home/fluidd/kiauh/kiauh.sh install fluidd-config
 }
