16abda3ccc376e1fbd2350f81e5d123a9234011d
kerbilg <44060099+kerbilg@users.noreply.github.com>
2026-09-06 03:28:48 +0200

    fix: create moonraker.conf

--- userpatches/extensions/kiauh.sh
+++ userpatches/extensions/kiauh.sh
@@ -10,7 +10,7 @@ function post_customize_image__011_install_stack() {
     chroot_sdcard_apt_get_install expect
     
     chroot_sdcard sudo -H -u fluidd env TERM=xterm bash -x /home/fluidd/kiauh/kiauh.sh install klipper --count 1 --create-example-cfg
-    chroot_sdcard sudo -H -u fluidd env TERM=xterm bash -x /home/fluidd/kiauh/kiauh.sh install moonraker
+    chroot_sdcard sudo -H -u fluidd env TERM=xterm bash -x /home/fluidd/kiauh/kiauh.sh install moonraker --create-example-cfg
     chroot_sdcard sudo -H -u fluidd env TERM=xterm bash -x /home/fluidd/kiauh/kiauh.sh install fluidd
     chroot_sdcard sudo -H -u fluidd env TERM=xterm bash -x /home/fluidd/kiauh/kiauh.sh install fluidd-config
 }
